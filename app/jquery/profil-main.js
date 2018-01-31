edbb75eea61b5cfaa6b352bea80584805efc05a2
skreets <mail@skreets.ru>
2018-01-31 03:45:44 +0300

    desktop sidebar

---
+++ app/jquery/profil-main.js
@@ -0,0 +1,15 @@
+
+
+$(window).on('load',function(){
+
+  $('.sidebar').overlayScrollbars({
+    className: 'os-theme-thin-light',
+    scrollbars: {
+      autoHide: 'leave',
+      autoHideDelay: 0,
+    }
+  });
+
+});
+
+// $(document).ready(function(){});
