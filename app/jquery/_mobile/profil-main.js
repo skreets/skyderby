764e5c062a1c529021d43bf26791b55d58a0167b
skreets <mail@skreets.ru>
2018-02-16 00:11:07 +0300

    анимация для мобильного sidebar

--- app/jquery/_mobile/profil-main.js
+++ app/jquery/_mobile/profil-main.js
@@ -4,4 +4,9 @@ $(document).ready(function(){
     $(this).toggleClass('active');
     $('.sidebar').toggleClass('active');
   });
+
+  $('body').on('click', '.section_header_burger', function() {
+    $(this).toggleClass('active');
+    $('.section_header, .section_header_nav').toggleClass('active');
+  });
 });
